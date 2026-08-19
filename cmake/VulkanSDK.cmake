05cae3ffcd196690879eb7030f2dcabd7d852c1b
tcoppex <tcoppex@proton.me>
2026-08-19 16:23:25 +0200

    cleanup

--- cmake/VulkanSDK.cmake
+++ cmake/VulkanSDK.cmake
@@ -36,7 +36,7 @@ endif()
 
 # -----------------------------------------------------------------------------
 
-## Custom function to generate binary shaders from GLSL, with include handling.
+## Custom function to generate binary shaders from GLSL.
 function(glsl2spirv input_glsl output_spirv shader_dir deps extra_args)
   # Retrieve the input file name
   get_filename_component(fn ${input_glsl} NAME)
@@ -68,26 +68,23 @@ function(glsl2spirv input_glsl output_spirv shader_dir deps extra_args)
   get_filename_component(output_dir ${output_spirv} DIRECTORY)
 
   # ----------------------------
-
-  # Destroy the output directory on clean if it was not created by the user
-  # beforehand.
-
+  # Create an identifer for the Output directory.
   string(MAKE_C_IDENTIFIER "${output_dir}" output_dir_id)
   set(var_name "${output_dir_id}_CREATED_BY_CMAKE")
-
   if(NOT DEFINED ${var_name})
     set(${var_name} OFF CACHE INTERNAL "Did CMake create ${output_dir}?")
   endif()
 
+  # Create the Output directory.
   if(NOT IS_DIRECTORY "${output_dir}")
     file(MAKE_DIRECTORY "${output_dir}")
     set(${var_name} ON CACHE INTERNAL "Did CMake create ${output_dir}?")
   endif()
 
+  # Mark it to be cleaned when it was not created by the user beforehand.
   if(${var_name})
     set_property(DIRECTORY APPEND PROPERTY ADDITIONAL_CLEAN_FILES "${output_dir}")
   endif()
-
   # ----------------------------
 
   if(NOT stage OR stage STREQUAL "")
@@ -192,9 +189,6 @@ function(compile_glsl_shaders
 endfunction()
 
 # -----------------------------------------------------------------------------
-# -----------------------------------------------------------------------------
-# (Work In Progress)
-# -----------------------------------------------------------------------------
 
 function(compile_slang_shaders
   GLOBAL_SLANG_DIR
@@ -239,6 +233,36 @@ endfunction()
 
 # -----------------------------------------------------------------------------
 
+## Compile all shaders (*.glsl / *.slang) from one directory to another.
+## Used by the framework.
+function(compile_shaders
+  GLOBAL_SHADER_DIR
+  GLOBAL_SPIRV_DIR
+  binaries
+  sources
+  extra_dir
+)
+  set(local_binaries "")
+  set(local_sources "")
+
+  compile_glsl_shaders(${GLOBAL_SHADER_DIR} ${GLOBAL_SPIRV_DIR} glsl_binaries glsl_sources ${extra_dir})
+  if(glsl_binaries)
+    list(APPEND local_binaries ${glsl_binaries})
+    list(APPEND local_sources ${glsl_sources})
+  endif()
+
+  compile_slang_shaders(${GLOBAL_SHADER_DIR} ${GLOBAL_SPIRV_DIR} slang_binaries slang_sources ${extra_dir})
+  if(slang_binaries)
+    list(APPEND local_binaries ${slang_binaries})
+    list(APPEND local_sources ${slang_sources})
+  endif()
+
+  set(${binaries} "${local_binaries}" PARENT_SCOPE)
+  set(${sources} "${local_sources}" PARENT_SCOPE)
+endfunction()
+
+# -----------------------------------------------------------------------------
+
 ## Compile all shaders (glsl and slang alike) from glsl/ and slang/ subdirectories.
 ## Used by the samples.
 function(compile_sample_shaders
@@ -268,35 +292,6 @@ function(compile_sample_shaders
       list(APPEND local_binaries ${slang_binaries})
       list(APPEND local_sources ${slang_sources})
     endif()
-  endif()
-
-  set(${binaries} "${local_binaries}" PARENT_SCOPE)
-  set(${sources} "${local_sources}" PARENT_SCOPE)
-endfunction()
-
-# -----------------------------------------------------------------------------
-
-## Compile all shaders (*.glsl / *.slang) from one directory to another.
-function(compile_shaders
-  GLOBAL_SHADER_DIR
-  GLOBAL_SPIRV_DIR
-  binaries
-  sources
-  extra_dir
-)
-  set(local_binaries "")
-  set(local_sources "")
-
-  compile_glsl_shaders(${GLOBAL_SHADER_DIR} ${GLOBAL_SPIRV_DIR} glsl_binaries glsl_sources ${extra_dir})
-  if(glsl_binaries)
-    list(APPEND local_binaries ${glsl_binaries})
-    list(APPEND local_sources ${glsl_sources})
-  endif()
-
-  compile_slang_shaders(${GLOBAL_SHADER_DIR} ${GLOBAL_SPIRV_DIR} slang_binaries slang_sources ${extra_dir})
-  if(slang_binaries)
-    list(APPEND local_binaries ${slang_binaries})
-    list(APPEND local_sources ${slang_sources})
   endif()
 
   set(${binaries} "${local_binaries}" PARENT_SCOPE)
