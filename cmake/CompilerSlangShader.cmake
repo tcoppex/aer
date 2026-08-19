f1dbb17f5ff78906f04c4cc69ff1664ab3a6e692
tcoppex <tcoppex@proton.me>
2026-08-19 11:38:22 +0200

    [CMake][Slang] Force default column major layout.
    -> remove `-matrix-layout-row-major` from _CORE_FLAGS

--- cmake/CompilerSlangShader.cmake
+++ cmake/CompilerSlangShader.cmake
@@ -223,7 +223,7 @@ function(compile_slang SHADER_FILES OUTPUT_DIR)
 
   set(_CORE_FLAGS
     -emit-spirv-directly
-    -matrix-layout-row-major
+    -matrix-layout-column-major # -matrix-layout-row-major
     -force-glsl-scalar-layout
     -fvk-use-entrypoint-name
   )
